4e7085af1b472be370d047238a8376746bcc633b
Luke Sorrenti <lrsorrenti@hotmail.com>
2024-06-19 08:14:19 -0500

    updated onclick for button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const Button = ({ styles }) => {
   return (
     <button
+      onClick={() => document.getElementById('cta-section').scrollIntoView({ behavior: 'smooth' })}
       type="button"
       className={`bg-blue-gradient px-6 py-4 font-poppins text-[18px] font-medium text-primary outline-none ${styles} rounded-[10px] transition-opacity duration-300 ease hover:opacity-70`}
     >
